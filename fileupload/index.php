5de586d230e7be73f8ee08aa9a65cb6b62441eac
Calvine Museywa Azenga <75707304+CalvineAzenga@users.noreply.github.com>
2023-04-20 15:38:46 +0300

    Add files via upload

---
+++ fileupload/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <table>
+    <tr><h2>Select image to upload:</h2></tr>
+    <tr>
+    <td><input type="file" name="fileToUpload" id="fileToUpload"></td>
+    <td><input type="submit" value="Upload Image" name="submit"></td>
+    </tr>
+    </table>  
+</form>
+</body>
+</html>
